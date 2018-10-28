b65bc2f0c54f8d5cd0fa6f4b7a14504afd1fe724
Blagoja Stojanovski <bstojanovski@dev.smartmerger.com>
2018-10-28 23:07:42 +0200

    Added missing migration

--- CarRental.Data/Migrations/20181028210529_ChangedNullableFieldsInRezervation.Designer.cs
+++ CarRental.Data/Migrations/20181028210529_ChangedNullableFieldsInRezervation.Designer.cs
@@ -4,14 +4,16 @@ using CarRental.Data;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 namespace CarRental.Data.Migrations
 {
     [DbContext(typeof(CarRentalDbContext))]
-    partial class CarRentalDbContextModelSnapshot : ModelSnapshot
+    [Migration("20181028210529_ChangedNullableFieldsInRezervation")]
+    partial class ChangedNullableFieldsInRezervation
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ CarRental.Data/Migrations/20181028210529_ChangedNullableFieldsInRezervation.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace CarRental.Data.Migrations
+{
+    public partial class ChangedNullableFieldsInRezervation : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<DateTime>(
+                name: "ReturnDate",
+                table: "Rezervations",
+                nullable: false,
+                oldClrType: typeof(DateTime),
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<DateTime>(
+                name: "PickUpDate",
+                table: "Rezervations",
+                nullable: false,
+                oldClrType: typeof(DateTime),
+                oldNullable: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<DateTime>(
+                name: "ReturnDate",
+                table: "Rezervations",
+                nullable: true,
+                oldClrType: typeof(DateTime));
+
+            migrationBuilder.AlterColumn<DateTime>(
+                name: "PickUpDate",
+                table: "Rezervations",
+                nullable: true,
+                oldClrType: typeof(DateTime));
+        }
+    }
+}
